63c143d46c314098273f3fc961f8ce7fa9b3864c
LuaanBarbosa <luancesar@outlook.com>
2022-03-09 12:01:48 -0300

    Inicio do projeto
    inicio do projeto de clone da página de resultados do google, foi usado como modelo o projeto-google para começar o desenvolvimento.

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+// window.onload = function(){
+//     document.querySelector(".menuMobile").addEventListener("click", function(){
+//         if (document.querySelector(".menu nav ul").style.display == 'flex'){
+//             document.querySelector(".menu nav ul").style.display = 'none';
+//         } else {
+//             document.querySelector(".menu nav ul").style.display = 'flex';
+//         }
+//     });
+// }
+
+// $("a.scroll").on('click', function(event) {
+//     if (this.hash !== "") {
+//         event.preventDefault();
+//         let hash = this.hash;
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 5000, function() {
+//             window.location.hash = hash;
+//         });
+//     }
+// });
